c4b01408ebb1ad79f1c3c06af9b11cf502c59b73
wendy <enyinnapc16127@gmail.com>
2022-06-07 17:49:02 -0700

    Add Prime number checker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 // complete the function
 function prime (num) {
   // code goes here
+
+  //My solution
+
+  // A prime number can have only two factors.
+  // Hence, 0 and 1 is not included
+
+  //2 is the only even number that is a prime number.
+
+  //Check if num equal 2
+
+  if (num===2) {
+    return true;
+  }
+
+  // check if num equal 0, 1
+
+  if (num < 2) {
+    return false;
+  }
+
+  // Looping through 2 to check if num is prime
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  } 
+  return true;
+  
 }
 
 function solution (arg) {
